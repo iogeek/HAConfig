e4f441d68593aa3bd31cbf83ab0faddc5ddfcc5d
Robbie Harris <iogeek@users.noreply.github.com>
2023-05-30 22:25:58 -0700

    eos

--- python_scripts/set_unit_state.py
+++ python_scripts/set_unit_state.py
@@ -1,8 +1,8 @@
 state = data.get("state")
 name = data.get("name")
-time_seconds = data.get("time_seconds")
 
-logger.info("set_unit_state state[%s] name[%s] timeS[%s]",state, name, time_seconds)
+
+logger.info("set_unit_state state[%s] name[%s]",state, name)
 if state == "off":
     hass.services.call('homeassistant', 'turn_off', {'entity_id': name})
     logger.info("set_unit_state turned off")
