dcfcae5c5351bf647f1ed3ae926428d01a56ace1
Robbie Harris <iogeek@users.noreply.github.com>
2023-06-02 20:52:51 -0700

    eos

--- python_scripts/set_unit_state.py
+++ python_scripts/set_unit_state.py
@@ -1,5 +1,5 @@
-state = data.get("state")
-name = data.get("name")
+
+msg = data.get("message")
 
 thisdict = {
   "brand": "Ford",
@@ -20,11 +20,11 @@ sixChannelMask = {
 }
 
 
-logger.info("set_unit_state state[%s] name[%s]",state, name)
-if state == "off":
-    hass.services.call('homeassistant', 'turn_off', {'entity_id': name})
-    logger.info("set_unit_state turned off")
-else:
-    hass.services.call('homeassistant', 'turn_on', {'entity_id': name})
-    logger.info("set_unit_state turned on")
+logger.info("set_unit_state state[%s] name[%s]",msg)
+#if state == "off":
+#    hass.services.call('homeassistant', 'turn_off', {'entity_id': name})
+#    logger.info("set_unit_state turned off")
+#else:
+#    hass.services.call('homeassistant', 'turn_on', {'entity_id': name})
+#    logger.info("set_unit_state turned on")
 logger.info("set_unit_state done")
