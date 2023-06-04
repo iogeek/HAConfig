92d701ecc2a673e2cefce178b6e87435325dac84
Robbie Harris <iogeek@users.noreply.github.com>
2023-06-04 08:14:44 -0700

    eos

--- python_scripts/esp_cmd.py
+++ python_scripts/esp_cmd.py
@@ -3,3 +3,21 @@ msg = data.get("message")
 if not msg:
     logger.info("esp-cmd msg not passed in ")
 logger.info("esp-cmd unit_state msg[%s]",msg)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+sixChannelArray = ["Ford", "Volvo", "BMW"]
+
+sixChannelMask = {
+  "0": "bit1:off,bit2:off,bit3:off",
+  "1": "bit1:on,bit2:off,bit3:off",
+  "2": "bit1:off,bit2:on,bit3:off",
+  "3": "bit1:on,bit2:on,bit3:off",
+  "4": "bit1:off,bit2:off,bit3:on",
+  "5": "bit1:on,bit2:off,bit3:on",
+  "6": "bit1:off,bit2:on,bit3:on"
+}
