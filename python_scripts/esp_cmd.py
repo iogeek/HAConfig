bfe98725bc0b74caf4af38cad01ff07ab9de1dd9
Robbie Harris <iogeek@users.noreply.github.com>
2023-06-06 01:47:24 -0700

    eos

--- python_scripts/esp_cmd.py
+++ python_scripts/esp_cmd.py
@@ -9,17 +9,17 @@ haUnits = {
     "z1_flower_path": "z1_main",
     "z1_berry_path": "z1_main",
     "z1_grid_drip": "z1_main",
-    "z1_grid1": "z1_grid12.sw1",
-    "z1_grid2":
-    "z1_grid3":
-    "z1_grid4":
-    "z1_grid5":
-    "z1_grid6":
-    "z1_grid7":
-    "z1_grid8":
-    "z1_grid9":
-    "z1_grid10":
-    "z1_bed1":
+    "z1_grid1": "z1_grid12_sw1",
+    "z1_grid2": "z1_grid12_sw2",
+    "z1_grid3": "z1_grid34_sw1",
+    "z1_grid4": "z1_grid34_sw2",
+    "z1_grid5": "z1_grid56_sw1",
+    "z1_grid6": "z1_grid56_sw2",
+    "z1_grid7": "z1_grid78_sw1",
+    "z1_grid8": "z1_grid78_sw2",
+    "z1_grid9": "z1_grid90_sw1",
+    "z1_grid10": "z1_grid90_sw2",
+    "z1_bed1": 
     "z1_bed2":
     "z1_bed3":
     "z1_grape_wall":
@@ -53,3 +53,5 @@ sixChannelMask = {
   "5": "bit1:on,bit2:off,bit3:on",
   "6": "bit1:off,bit2:on,bit3:on"
 }
+
+# switch.test_1_channel_sw1
