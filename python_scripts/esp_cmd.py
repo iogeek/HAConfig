bb672d5a098c4967e4ccec0b60593658a226ea3a
Robbie Harris <iogeek@users.noreply.github.com>
2023-06-06 06:56:39 -0700

    eos

--- python_scripts/esp_cmd.py
+++ python_scripts/esp_cmd.py
@@ -2,11 +2,22 @@
 msg = data.get("message")
 if not msg:
     logger.info("esp-cmd msg not passed in ")
+    quit()
 logger.info("esp-cmd unit_state msg[%s]",msg)
 
 haStateDict = {
     "on": "turn_on",
     "off": "turn_off"
+}
+
+sixChannelMask = {
+  "0": "bit1:off,bit2:off,bit3:off",
+  "1": "bit1:on,bit2:off,bit3:off",
+  "2": "bit1:off,bit2:on,bit3:off",
+  "3": "bit1:on,bit2:on,bit3:off",
+  "4": "bit1:off,bit2:off,bit3:on",
+  "5": "bit1:on,bit2:off,bit3:on",
+  "6": "bit1:off,bit2:on,bit3:on"
 }
 
 ha6cUnitDict = {
@@ -36,36 +47,32 @@ haUnitDict = {
     "z1_grid9": "z1_grid90_sw1",
     "z1_grid10": "z1_grid90_sw2",
     "z1_bed1":  "z1_bed1_sw1",
-    "z1_bed2":
-    "z1_bed3":
-    "z1_grape_wall":
+    "z1_bed2": "z1_bed2_sw1",
+    "z1_bed3": "z1_bed3_sw1",
+    "z1_grape_wall": "z1_grape_wall_sw1",
     #z2 units
-    "z2_rosemary"
-    "z2_bay_bush"
-    "z2_grape"
-    "z2_big_pear"
-    "z1_little_pear"
-    "z2_cherry"
-    "z2_apple"
-    "z2_tub1"
-    "z2_tub2"
-    "z2_tub3"
-    "z2_tub4"
-    "z2_tub5"
-    "z2_tub6"
-    "z2_tub7"
+    "z2_rosemary": "z2_rosemary_sw1",
+    "z2_bay_bush": "z2_bay_bush_sw1",
+    "z2_grape": "z2_grape_sw1",
+    "z2_big_pear":  "z2_big_pear_sw1",
+    "z2_little_pear": "z2_little_pear_sw1",
+    "z2_cherry" : "z2_cherry_apple_sw1",
+    "z2_apple" : "z2_cherry_apple_sw2",
+    "z2_tub1" : "z2_tub12_sw1",
+    "z2_tub2" : "z2_tub12_sw2",
+    "z2_tub3" : "z2_tub34_sw1",
+    "z2_tub4" : "z2_tub34_sw2",
+    "z2_tub5" : "z2_tub56_sw1",
+    "z2_tub6" : "z2_tub56_sw2",
+    "z2_tub7" : "z2_tub7_sw1"
 }
 
-sixChannelArray = ["Ford", "Volvo", "BMW"]
+deviceStrList = msg.split(",")
 
-sixChannelMask = {
-  "0": "bit1:off,bit2:off,bit3:off",
-  "1": "bit1:on,bit2:off,bit3:off",
-  "2": "bit1:off,bit2:on,bit3:off",
-  "3": "bit1:on,bit2:on,bit3:off",
-  "4": "bit1:off,bit2:off,bit3:on",
-  "5": "bit1:on,bit2:off,bit3:on",
-  "6": "bit1:off,bit2:on,bit3:on"
-}
+for deviceStr in deviceStrList:
+  deviceInfo = deviceStr.split(":")
+  device = deviceInfo[0]
+  state = haStateDict[deviceInfo[1]]
+  print f
 
 # switch.test_1_channel_sw1
